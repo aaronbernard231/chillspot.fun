aae00ad77e3076a04b0735edea47b0f81ad619d0
Aaron Bernard <aaronbernard@Aarons-MBP.home>
2022-08-05 21:09:04 -0400

    d
    d

--- js/scripts.js
+++ js/scripts.js
@@ -48,7 +48,7 @@ window.addEventListener('DOMContentLoaded', event => {
     var pl = '';
      if(r.players.sample.length > 0 ){ pl = '<br>OP: '+r.players.sample[0].name;  }
       $('#rest').html(r.description.replace(/§(.+?)/gi, '')+'<br><b>Players Online:</b> '+r.players.online+pl);
-     $('#favicon').attr('src', r.favicon);
+
 
     });
     // Collapse responsive navbar when toggler is visible
